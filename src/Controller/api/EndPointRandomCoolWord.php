a8edd86684174b7eba76df218556c9293849734d
Miriam Lopez <miriam.lm@students.salle.url.edu>
2020-05-07 21:45:50 +0200

    delete unused use in EndPointRandomCoolWordWithRandomColor

--- src/Controller/api/EndPointRandomCoolWord.php
+++ src/Controller/api/EndPointRandomCoolWord.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace LaSalle\ChupiProject\Controller\api;
 
-
-use LaSalle\ChupiProject\Module\Color\Domain\RandomColorSearcher;
-use LaSalle\ChupiProject\Module\Color\Infrastructure\InMemoryColorRepository;
 use LaSalle\ChupiProject\Module\CoolWord\Domain\RandomCoolWordSearcher;
 use LaSalle\ChupiProject\Module\CoolWord\Infrastructure\InMemoryCoolWordRepository;
 use Symfony\Component\HttpFoundation\Request;
