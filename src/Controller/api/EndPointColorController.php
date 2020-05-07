d5ff32129eaaee86f0bf8b5c90412d98477abcc3
Miriam Lopez <miriam.lm@students.salle.url.edu>
2020-05-07 21:27:32 +0200

    add call to application service RandomColorSearcher to get de random color

--- src/Controller/api/EndPointColorController.php
+++ src/Controller/api/EndPointColorController.php
@@ -5,7 +5,20 @@ declare(strict_types=1);
 namespace LaSalle\ChupiProject\Controller\api;
 
 
+use LaSalle\ChupiProject\Module\Color\Domain\RandomColorSearcher;
+use LaSalle\ChupiProject\Module\Color\Infrastructure\InMemoryColorRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 final class EndPointColorController
 {
+    public function __invoke(Request $request): Response
+    {
+        $inMemoryColorRepository = new InMemoryColorRepository();
+        $randomColorSearcherService= new RandomColorSearcher($inMemoryColorRepository);
 
+        $randomColorSearcher = $randomColorSearcherService();
+
+        return Response::create($randomColorSearcher, 200);
+    }
 }
