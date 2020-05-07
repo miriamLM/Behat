2c0e08a67e24f906bea4a979af670f18a7fce956
Miriam Lopez <miriam.lm@students.salle.url.edu>
2020-05-07 21:26:29 +0200

    add routing in chupi.php

--- chupi.php
+++ chupi.php
@@ -1,6 +1,14 @@
 <?php require 'vendor/autoload.php';
 
 use Colors\Color;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\HttpFoundation\Request;
+use LaSalle\ChupiProject\Controller\api\EndPointColorController;
 use LaSalle\ChupiProject\Module\Color\Domain\RandomColorSearcher;
 use LaSalle\ChupiProject\Module\Color\Infrastructure\InMemoryColorRepository;
 use LaSalle\ChupiProject\Module\CoolWord\Domain\RandomCoolWordSearcher;
@@ -25,3 +33,30 @@ function _random_color_except(string $except, callable $randomColorSearcher): st
 
     return $frontColor;
 }
+
+
+$routes = [
+    'color'   => (new Route('/color',           ['controller' => EndPointColorController::class]))->setMethods([Request::METHOD_GET]),
+    'word'  => (new Route('/word', ['controller' => UserController::class]))->setMethods([Request::METHOD_GET])
+];
+
+$rc = new RouteCollection();
+foreach ($routes as $key => $route) {
+    $rc->add($key, $route);
+}
+$context = new RequestContext();
+$matcher = new UrlMatcher($rc, $context);
+$request = Request::createFromGlobals();
+$context->fromRequest($request);
+
+try {
+    $attributes = $matcher->match($context->getPathInfo());
+    $ctrlName = $matcher->match($context->getPathInfo())['controller'];
+    $ctrl = new $ctrlName();
+    $request->attributes->add($attributes);
+    $response = $ctrl($request);
+} catch (ResourceNotFoundException $e) {
+    $response = new Response('Not found!', Response::HTTP_NOT_FOUND);
+}
+
+$response->send();
