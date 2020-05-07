22f7d51aed04354720bf5c490eae048335064c5d
Miriam Lopez <miriam.lm@students.salle.url.edu>
2020-05-07 22:00:27 +0200

    add invoke method to the EndPointRandomCoolWordWithRandomColor class with the code of chupi.php that generates a cool word with different colors for font and background

--- src/Controller/api/EndPointRandomCoolWordWithRandomColor.php
+++ src/Controller/api/EndPointRandomCoolWordWithRandomColor.php
@@ -4,8 +4,32 @@ declare(strict_types=1);
 
 namespace LaSalle\ChupiProject\Controller\api;
 
+use Colors\Color;
+use LaSalle\ChupiProject\Module\Color\Domain\RandomColorSearcher;
+use LaSalle\ChupiProject\Module\Color\Infrastructure\DifferentRandomColorExpectGenerator;
+use LaSalle\ChupiProject\Module\Color\Infrastructure\InMemoryColorRepository;
+use LaSalle\ChupiProject\Module\CoolWord\Domain\RandomCoolWordSearcher;
+use LaSalle\ChupiProject\Module\CoolWord\Infrastructure\InMemoryCoolWordRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 final class EndPointRandomCoolWordWithRandomColor
 {
+    public function __invoke(Request $request): Response
+    {
+        $wordSearcher  = new RandomCoolWordSearcher(new InMemoryCoolWordRepository());
+        $colorSearcher = new RandomColorSearcher(new InMemoryColorRepository());
+
+        $bgColor = $colorSearcher();
+
+        $differentColorGenerator= new DifferentRandomColorExpectGenerator();
+        $fgColor = $differentColorGenerator->random_color_except($bgColor, $colorSearcher);
+
+        $color = new Color();
+        $coolwordwithstyle=$color($wordSearcher())->bg($bgColor)->$fgColor . PHP_EOL;
+
+
+        return Response::create($coolwordwithstyle, 200);
+    }
 
 }
