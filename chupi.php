7167e9db93cedf9abd7e574fc7c5dcc7f4801d38
Miriam Lopez <miriam.lm@students.salle.url.edu>
2020-05-07 20:35:47 +0200

    change variable name $c for $color in chupi.php

--- chupi.php
+++ chupi.php
@@ -12,8 +12,8 @@ $colorSearcher = new RandomColorSearcher(new InMemoryColorRepository());
 $bgColor = $colorSearcher();
 $fgColor = _random_color_except($bgColor, $colorSearcher);
 
-$c = new Color();
-echo $c($wordSearcher())->bg($bgColor)->$fgColor . PHP_EOL;
+$color = new Color();
+echo $color($wordSearcher())->bg($bgColor)->$fgColor . PHP_EOL;
 
 function _random_color_except(string $except, callable $randomColorSearcher): string
 {
