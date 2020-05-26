84b4654b505c6334b3edfd1c3cda30c9cb1dca80
Miriam Lopez <miriam.lm@students.salle.url.edu>
2020-05-26 23:31:07 +0200

    add strict_types

--- chupi.php
+++ chupi.php
@@ -1,4 +1,8 @@
-<?php require 'vendor/autoload.php';
+<?php
+
+declare(strict_types=1);
+
+require 'vendor/autoload.php';
 
 use LaSalle\ChupiProject\Controller\api\EndPointRandomCoolWord;
 use LaSalle\ChupiProject\Controller\api\EndPointRandomCoolWordWithRandomColor;
@@ -19,21 +23,29 @@ $routes = [
 ];
 
 $routeCollection = new RouteCollection();
+
 foreach ($routes as $key => $route) {
     $routeCollection->add($key, $route);
 }
+
 $requestContext = new RequestContext();
 $matcher = new UrlMatcher($routeCollection, $requestContext);
+
 $request = Request::createFromGlobals();
 $requestContext->fromRequest($request);
 
 try {
+
     $attributes = $matcher->match($requestContext->getPathInfo());
     $ctrlName = $matcher->match($requestContext->getPathInfo())['controller'];
+
     $ctrl = new $ctrlName();
+
     $request->attributes->add($attributes);
     $response = $ctrl($request);
+
 } catch (ResourceNotFoundException $e) {
+
     $response = new Response('Not found!', Response::HTTP_NOT_FOUND);
 }
 
